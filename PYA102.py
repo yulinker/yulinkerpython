7479c0d8118ab96cb7287d0a506edad0d8612acd
yulinker <91936527+yulinker@users.noreply.github.com>
2021-10-05 16:14:32 +0800

    add pya2

---
+++ PYA102.py
@@ -0,0 +1,14 @@
+# TODO
+a=eval(input())
+b=eval(input())
+c=eval(input())
+d=eval(input())
+
+# 靠右對齊
+# TODO
+print("|{0:>7.2f} {1:>7.2f}|".format(a,b))
+print("|{0:>7.2f} {1:>7.2f}|".format(c,d))
+# 靠左對齊
+# TODO
+print("|{0:<7.2f} {1:<7.2f}|".format(a,b))
+print("|{0:<7.2f} {1:<7.2f}|".format(c,d))
