3f657aa955752a51c06a54df2eb1c5535a8684f9
Yulin <ykuo088@gmail.com>
2021-10-05 15:13:59 +0800

    new py file

---
+++ PYA101.py
@@ -0,0 +1,15 @@
+#TODO
+
+#向右靠齊
+#TODO
+
+#向左靠齊
+#TODO
+a=eval(input())
+b=eval(input())
+c=eval(input())
+d=eval(input())
+print("|{0:>5d} {1:>5d}|".format(a,b))
+print("|{0:>5d} {1:>5d}|".format(c,d))
+print("|{0:<5d} {1:<5d}|".format(a,b))
+print("|{0:<5d} {1:<5d}|".format(c,d))
